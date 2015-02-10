b5eb6fe81313bac6ddc5a5ec98fc1e6f0a75c0ef
Meng Lei <raymon.ph@foxmail.com>
2015-02-09 17:07:18 -0800

    temp push
    in case anything lost

---
+++ Meng/main.cpp
@@ -0,0 +1,12 @@
+// Author: Meng Lei
+// PDP-8 memory simulator
+// main.cpp: main functions verification and testing
+
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+int main()
+{
+	
+}
