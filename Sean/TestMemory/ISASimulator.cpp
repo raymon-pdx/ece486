a896c6d4280b421cd350c64a6a2ce2108663f074
Sean Hendrickson <henamber3@aol.com>
2015-02-10 17:43:41 -0800

    testing storing and retrieving data from memory.  First merger of our group's code

---
+++ Sean/TestMemory/ISASimulator.cpp
@@ -0,0 +1,103 @@
+/*
+* AUTHORS: Sean Hendrickson, 
+* FILE: ISASimulator.cpp
+* LAST MODIFIED: 1/20/2015
+* DESCRIPTION: Tests parsing octal input file for ISA simulator
+*/
+#include "parser.h"
+#include "memory.h"
+using namespace std;
+
+#define TEST_FILE "test2.txt" // used for debugging
+#define OUTPUT_FILE "clean2.txt"  // clean version of text file 
+#define NUM_PAGES 32  // number of pages in memory
+#define PAGE_CAPACITY 128 // number of addresses per page
+#define STARTING_ADDRESS 128 // where program is stored
+
+int main(int argc, char *argv[])
+{
+	ifstream myFile;    // hold trace file
+	ofstream outputFile;  // show cleaned file
+	int loadOffset = 0;  // offset used to load data from text file
+	int opcode = 0;
+	bool I = false;
+	bool M = false;
+	int offset = 0;
+	int lineCount = 0;
+	int address = 0;
+	int id = 0;
+	int ret = 0;
+	int count = 0;
+	
+	// check for program error
+	if (argc < 1)
+	{
+		std::cout << "ERROR - program execution\n";
+	}
+
+	myFile.open(TEST_FILE);
+	outputFile.open(OUTPUT_FILE);
+	pagetable Memory(NUM_PAGES, PAGE_CAPACITY);  // initialize memory
+	
+	// write to an output file to see the cleaned file
+	while (!myFile.eof())
+	{
+		++count;
+
+		cout << "DATA " << count << endl;
+		cout << "Address before = " << address << endl;
+		cout << "id before = " << id << endl;
+		ret = getAddress(myFile, id, address, lineCount);
+		cout << "Address after = " << address << endl;
+		cout << "id after = " << id << endl;
+
+		if (ret < 0)
+		{   // handle error and exit
+			cout << "ERROR - failed to get address on " << count << "th attempt\n";
+			goto EXIT;
+		}
+		if (ret > 0)
+		{   // handle error and exit
+			cout << "ERROR found on line" << lineCount << endl;
+			goto EXIT;
+		}
+
+		outputFile << id << "  " << address << endl;  // write address to file
+
+		// break down the address
+		ret = parseAddress(address, opcode, I, M, offset);
+		if (ret < 0)
+		{   // handle error and exit
+			cout << "ERROR - failed to parse address on " << count << "th attempt\n";
+			goto EXIT;
+		}
+
+		outputFile << "[ " << opcode << " | " << I << " | " << M << " | " << offset << " ]\n";
+
+		cout << "Address = " << STARTING_ADDRESS + loadOffset << ", Data passed = " << address << endl;
+
+		// add data to octal address 200
+		if (Memory.store(STARTING_ADDRESS + loadOffset, address) < 0)
+		{
+			cout << "ERROR - adding data failed\n";
+			goto EXIT;
+		}
+
+		if (ret = Memory.load(STARTING_ADDRESS + loadOffset) != address)
+		{
+			cout << "ERROR - memory does not match passed data\n";
+			goto EXIT;
+		}
+		cout << "Data retrieved = " << ret << endl << endl;
+
+		++loadOffset;
+	}
+	
+EXIT:
+	// close the file
+	myFile.close();
+	outputFile.close();
+
+	exitMessage();
+	return 0;
+}	
---
+++ Sean/TestMemory/parser.h
@@ -0,0 +1,97 @@
+/*
+* AUTHORS: Sean Hendrickson,
+* FILE: parser.h
+* LAST MODIFIED: 2/09/2015
+* DESCRIPTION: Functions to parse information from text file
+*/
+
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+
+#define DEBUG 0   // 1 for on, 0 for off
+#define ADDRESS_SIZE 12  // number of bits in address
+#define OFFSET_SIZE 7  // size of offset used for bitset
+#define MAX_NUMBERS 3  // number of numbers to find per line
+#define NUM_OPCODE_BITS 3 // number of bits used for opcode
+
+/* FUNCTION PROTOTYPES */
+
+// load the file into memory
+// PRE-COND: file must already be opened
+// POST-COND: value of id and address returned 
+//            through corresponding variables
+// PARAM: file is a string of the file to be accessed
+// RET: 0 if successful
+//      -1 if failed
+int getAddress(std::ifstream &file, int &id, int &address, int &lineCount);
+
+
+// break down an octal address into opcode, I, M, and offset bits
+// PRE-COND: opcode, I, M, and offset must be declared
+// POST-COND: data passed back through parameters
+// RET: 0 on success
+//     -1 if an error occured
+int parseAddress(int address, int &opcode, bool &I, bool &M, int &offset);
+
+
+// this function takes a string of octal values and 
+// converts it to a binary sting
+// PRE-COND: characters must be ANSI
+//           characters must be octal
+// return: EMPTY if error occured
+std::string octalToBinary(std::string octal);
+
+
+// this function takes a string of octal values and 
+// converts it to an integer
+// PRE-COND: characters must be ANSI
+//           characters must be octal
+// return: -1 if error occured
+int octalToInt(std::string octal);
+
+
+// this function generates a binary string based
+// on the number of opcode bits
+// PRE-COND: can only be used to compare opcode
+// param: value - binary value of string
+// return: string of NUM_OPCODE_BITS bits long
+std::string genOpcodeString(int value);
+
+
+// this function generates a binary string based
+// on the pre-determined address size 
+// PRE-COND: size of string must be decided before
+//           compile time
+// param: value - binary value of string
+// return: string of ADDRESS_SIZE bits long
+std::string createBinaryString(int value);
+
+
+// returns the opcode from the address
+// PRE-COND: opcode must be a binary string
+// RET: int of opcode if found
+//      -1 if error occured
+int findOpcode(std::string address);
+
+
+// convert OFFSET_SIZE segment of binary string to an integer.
+// note that the offset is the character at the end of the string
+// PRE-COND: OFFSET_SIZE and ADDRESS_SIZE must already be set
+// PARAM: binaryAddress - string of 1's and 0's with ADDRESS SIZE
+//                        number of characters
+// RET: offset as and integer
+int getOffset(std::string binaryAddress);
+
+
+// this function returns true if a character is 
+// an octal value. Else false is returned
+bool isOctal(char character);
+
+
+// used to pause program to read debugging messages
+void exitMessage();
